b1242411cd871f855d114e682a9839e3b55fff4e
Ludvig Malm <malmludvig@gmail.com>
2025-01-29 16:00:43 +0100

    Update forms.php

--- forms.php
+++ forms.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Auto refresh
+    <!-- <meta http-equiv="refresh" content="5"> -->
+    
     <title>Formulär PHP</title>
 </head>
 <body>
